86ebeabac0b82dcf8f27101ace34a32e47bc1f7e
Iban Lopez <ibanlopezd@gmail.com>
2021-09-01 18:53:45 -0300

    ADDED: JWT middleware

---
+++ middleware/authJwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+verifyToken = (req, res, next) => {
+    let token = req.header('x-auth-token')
+
+    if (!token)  {
+        return res.send({ message: "No token provided" })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (req.user = decoded.user) {
+            next()
+        }
+    } catch (err) {
+        res.send({ msg: err.message || 'Token is not valid' })
+    }
+}
