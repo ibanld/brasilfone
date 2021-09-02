31c5f6156254195c6b3f69d648d3a954cf2bc44c
Iban Lopez <ibanlopezd@gmail.com>
2021-09-02 12:06:59 -0300

    DELETED: server rendering page

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
-app.get('/', (req, res) => res.send('Hello World'))
 app.use('/api/users', require('./routes/api/user.route'))
 app.use('/api/auth', require('./routes/api/auth.route'))
 
