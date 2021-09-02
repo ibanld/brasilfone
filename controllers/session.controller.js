5465643d70bbdf0b7950e78747aa4c2e7d397e46
Iban Lopez <ibanlopezd@gmail.com>
2021-09-02 12:08:03 -0300

    ADDED: comment on jwt

--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 exports.newSession = async (req, res) => {
     try {
         const email = req.body.email
+        // Use email of logged user to build token
         const token = await jwt.sign({ email: email }, process.env.JWT_SECRET)
         if (token) {
             return res.send(token)
