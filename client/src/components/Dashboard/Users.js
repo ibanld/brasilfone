aaa78f49e7e90c24d9dc03294ce5fa4e9d90e417
Iban Lopez <ibanlopezd@gmail.com>
2021-09-02 18:26:44 -0300

    ADDED: Users component page

--- client/src/components/Dashboard/Users.js
+++ client/src/components/Dashboard/Users.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useUsers, useDispatchUsers } from '../../context/usersContext'
+import { useAuth } from '../../context/authContext'
+import API from '../../utils/axios'
+import { Container, Table, Button, Input } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
+
+const tableColor= {
+    backgroundColor: '#17223B'
+}
 
 export default function Users() {
+
+    const { users, loading } = useUsers()
+    const { token } = useAuth()
+
+    const dispatchUsers = useDispatchUsers()
+
+    const loadUsers = async () => {
+        try {
+            const res = await API.get('users', {headers: {'x-auth-token': token}})
+            if (res) {
+                dispatchUsers({
+                    type: 'LOAD_USERS',
+                    payload: res.data
+                })
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    const handleDelete = async id => {
+        try {
+            const res = await API.delete(`users/${id}`, {headers: {'x-auth-token': token}})
+            if (res) {
+                dispatchUsers({
+                    type: 'DELETE_USER',
+                    payload: id
+                })
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    useEffect( ()=> {
+        let isSubscribed = true
+        if (isSubscribed) {
+            loadUsers()
+        }
+
+        return () => isSubscribed = false
+    }, [])
+
     return (
-        <div>
-            users
-        </div>
+        <Container text>
+            {loading ? 'loading' : 
+                <Table inverted style={tableColor}>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>ID</Table.HeaderCell>
+                            <Table.HeaderCell>E-Mail</Table.HeaderCell>
+                            <Table.HeaderCell>REGISTRO</Table.HeaderCell>
+                            <Table.HeaderCell>EXCLUIR</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {users.reverse().map( user => 
+                            <Table.Row key={user.id}>
+                                <Table.Cell>{user.id}</Table.Cell>
+                                <Table.Cell>{user.email}</Table.Cell>
+                                <Table.Cell>
+                                    <Moment format="DD/MM/YYYY">{user.createdAt}</Moment>
+                                </Table.Cell>
+                                <Table.Cell>
+                                    <Button negative icon="trash" onClick={ ()=> handleDelete(user.id)} />
+                                </Table.Cell>
+                            </Table.Row>    
+                        )}
+                    </Table.Body>
+                </Table>
+            }
+            <Link to="/dashboard">
+                <Button color="teal" compact icon="arrow left" float="left" content="Voltar" />
+            </Link>
+        </Container>
     )
 }
