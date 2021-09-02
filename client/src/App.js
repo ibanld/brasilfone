4c929a3eb11131b44a2dd7a7ad0f5a0ec8afd407
Iban Lopez <ibanlopezd@gmail.com>
2021-09-02 14:26:11 -0300

    ADDED: basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import LandingPage from './components/LandingPage/LandingPage'
-import Layout from './components/Layout';
+import Layout from './components/Layout'
+import LandingPage from './components/LandingPage'
+import Dashboard from './components/Dashboard'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
       <Layout>
-        <LandingPage />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </Router>
       </Layout>
   )
 }
---
+++ client/src/components/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div>
+            a Dashboard
+        </div>
+    )
+}
+
+export default Dashboard
