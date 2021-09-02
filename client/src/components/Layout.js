6050e0248bdebc8c96efa875f5f8f53399e0cf5e
Iban Lopez <ibanlopezd@gmail.com>
2021-09-02 13:28:20 -0300

    ADDED: NavBar v1 to Layout

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,17 +1,34 @@
+import { Menu, Icon } from 'semantic-ui-react'
 
 const style = {
     backgroundColor: '#263859', 
-    display: 'flex', 
-    justifyContent: 'center', 
-    alignItems: 'center',
     width: '100vw',
     height: '100vh'
+}
+
+const childrenStyle = {
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+}
+const menuStyle = {
+    backgroundColor: '#17223B'
 }
 
 export default function Layout({ children }) {
     return (
         <div style={style} >
-            { children }
+            <Menu inverted style={menuStyle}>
+                <Menu.Item>
+                    <Icon name="js square"/>
+                </Menu.Item>
+                <Menu.Item>
+                    Brasilfone - Challenge
+                </Menu.Item>
+            </Menu>
+            <div style={childrenStyle}>
+                { children }
+            </div>
         </div>
     )
 }
