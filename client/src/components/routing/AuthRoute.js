c6b4ab232625e416e0c8a138600d0470ecd8205f
Iban Lopez <ibanlopezd@gmail.com>
2021-09-02 15:05:51 -0300

    ADDED: Auth routes with logged context state

--- client/src/components/routing/AuthRoute.js
+++ client/src/components/routing/AuthRoute.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import {  useAuth } from '../../context/authContext'
 
 function AuthRoute({ component: Component,  ...rest }) {
-    
+    const { isLogged } = useAuth()
+
     return (
-        <Route {...rest} render={ (props) => ('ADD CONDITION' ? <Redirect to='/' /> : <Component {...props} />)} />
+        <Route {...rest} render={ (props) => (isLogged ? <Redirect to='/' /> : <Component {...props} />)} />
     )
 }
 export default AuthRoute
