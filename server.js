a7839a613f20022ffbffd964b8b38fea4219408d
Iban Lopez <ibanlopezd@gmail.com>
2021-09-03 17:27:02 -0300

    FIXED: deployment issue

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ app.use('/api/auth', require('./routes/api/auth.route'))
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production') {
     // Set the static folder
-  app.use(express.static(path.join(__dirname, 'public/build')));
+    app.use(express.static('client/build'));
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'build', 'index.html'));
-  });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 
