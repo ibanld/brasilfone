5c84dae299228f4f2147a325ccf218f32edff4c8
Iban Lopez <ibanlopezd@gmail.com>
2021-09-03 17:09:37 -0300

    FIXED: deployment error

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if(process.env.NODE_ENV === 'production') {
     // Set the static folder
   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/', function (req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
